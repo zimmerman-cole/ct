def249419b756686b125efdda2bba558c2e64043
Cole Zimmerman <czimmer112@gmail.com>
2017-05-31 16:36:22 +0100

    Initial commit

---
+++ approx.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Methods for finding low-rank approximation of matrices
+
+# Everything here is based on pseudocode from
+# "Finding structure with randomness: Probabilistic algorithms for
+# constructing approximate matrix decompositions"
+#       by N. Halko, P.G. Martinsson, and J.A. Tropp
+# https://arxiv.org/pdf/0909.4061.pdf
